09dccc6a5f3935b98850c41e32657b3200c362e2
Alex <aprostya@yandex.ru>
2018-10-15 11:34:14 +0300

    delete useless components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import BackgroundChange from './BackgroundChange';
+import './static/css/styles.css';
+import 'normalize.css'; 
 ReactDOM.render(<BackgroundChange />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
